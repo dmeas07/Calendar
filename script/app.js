2e59c0100d41b0b1aa1b6d9876c39bd7ac20a51b
dmeas07 <110435805+dmeas07@users.noreply.github.com>
2022-09-26 13:19:55 -0700

    second update

--- script/app.js
+++ script/app.js
@@ -6,57 +6,99 @@ var isImportant = false;
 
 var visible = true;
 
-function iconClick(){
-    if(isImportant  == false){
-        // change to important
-        $("#btnChangeIcon").removeClass(nonImportantIcon).addClass(importantIcon);
-        isImportant = true;
-        console.log("is important")
+function iconClick() {
+  if (isImportant == false) {
+    // change to important
+    $("#btnChangeIcon").removeClass(nonImportantIcon).addClass(importantIcon);
+    isImportant = true;
+    console.log("is important");
+  } else {
+    // change to non important
+    $("#btnChangeIcon").removeClass(importantIcon).addClass(nonImportantIcon);
+    isImportant = false;
+    console.log("is not important");
+  }
+}
+
+function hide() {
+  if (visible) {
+    $("#taskList").hide();
+
+    visible = false;
+
+    console.log("hidden");
+  } else {
+    $("#taskList").show();
+
+    visible = true;
+
+    console.log("reveal");
+  }
+}
+
+function saveTask() {
+  console.log("button clicked");
+
+  let title = $("#txtTitle").val();
+  let date = $("#txtDate").val();
+  let description = $("#txtDescription").val();
+  let tag = $("#txtTag").val();
+  let color = $("#txtColor").val();
+  let category = $("#txtCategory").val();
+
+  let task = new Task(title, date, description, tag, color, category);
+
+  displayTask(task);
+
+  clearForm();
+}
+
+function clearForm() {
+  $("#txtTitle").val("");
+  $("#txtDate").val("");
+  $("#txtDescription").val("");
+  $("#txtTag").val("");
+  $("#txtColor").val("");
+  $("#txtCategory").val("");
+}
+
+function displayTask(task) {
+  console.log(task);
+
+  let syntax = `<div class="task">
+
+    <div class="infoTask">
+      <h3>${task.title}</h3>
+      <p>${task.description}</p>
+    </div>
     
-    }
+    <label class="date">
+      ${task.date}
+    </label>
 
-    else{
-        // change to non important
-        $("#btnChangeIcon").removeClass(importantIcon).addClass(nonImportantIcon);
-        isImportant = false;
-        console.log("is not important")
-    }
+    <div class="extra">
+      <label>${task.category}</label>
+      <label>${task.tag}</label>
+    </div>
+
+    </div>`;
+
+  $("#taskList").append(syntax);
 }
 
-function hide(){
-    if(visible){
+function changeIcon() {}
 
-        $("#hideMe").hide(".paragraph");
+function toggleSelection() {}
 
-        visible = false;
+function init() {
+  console.log("Task Manager");
 
-        console.log("hidden");
+  // load prev data
 
-    }
-
-    else{
-        
-        $("#hideMe").show(".paragraph");
-
-        visible = true;
-        
-        console.log("reveal");
-    }
-}
-
-function saveTask(){
-    console.log("button clicked");
-}
-function init(){
-    console.log("Task Manager");
-
-    // load prev data
-
-    // catch events
-    $(".btnSave").click(saveTask);
-    $("#btnChangeIcon").click(iconClick);
-    $("#hideBtnBtn").click(hide)
-
+  // catch events
+  $("#btnSave").click(saveTask);
+  $("#btnChangeIcon").click(iconClick);
+  $("#hideBtnBtn").click(hide);
 }
 
 window.onload = init;
---
+++ script/task.js
@@ -0,0 +1,14 @@
+
+class Task {
+    constructor(title, date, desc, tag, color, cat){
+        this.title = title;
+        this.date = date;
+        this.description = desc;
+        this.tag = tag;
+        this.color - color;
+        this.category = cat;
+
+        this.name = "Daravy";
+
+    }
+}
